8cfaa2732164b186faa9e03185766a6551d7f5a6
Kate-sdet <kate1991k@gmail.com>
2025-09-08 15:25:34 -0500

    3rd commit

---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("dhd");
+        int f = 1;
+        System.out.println(5);
+    }
+}
--- src/main/java/tes.java
+++ src/main/java/tes.java
@@ -1,7 +1,7 @@
 public class tes {
     public static void main(String[] args) {
         System.out.println(5);
-     
+
         int a =5;
         String b = "ABC";
         System.out.println(a+b);
