0ed79d64c17067a3313e07132b362cdd2cd90dae
Kate-sdet <kate1991k@gmail.com>
2025-09-08 16:15:50 -0500

    5th commi

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -3,5 +3,9 @@ package org.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("this is project version 4");
+
+        System.out.println(23);
+        System.out.println("hello");
     }
+
 }
