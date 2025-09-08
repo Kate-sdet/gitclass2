e423f24f2e3bbf33a54b72b73336aeadd4805f0b
Kate-sdet <kate1991k@gmail.com>
2025-09-08 15:24:19 -0500

    2d commit

--- src/main/java/tes.java
+++ src/main/java/tes.java
@@ -1,6 +1,9 @@
 public class tes {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
+        System.out.println(5);
+     
+        int a =5;
+        String b = "ABC";
+        System.out.println(a+b);
     }
 }
