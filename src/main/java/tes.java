ee2af2a6bef3eb661ea214d977d473be60f69d8e
Kate-sdet <kate1991k@gmail.com>
2025-09-09 11:44:54 -0500

    new commit

--- src/main/java/tes.java
+++ src/main/java/tes.java
@@ -1,5 +1,7 @@
 public class tes {
     public static void main(String[] args) {
         System.out.println("This is version 6 made by person 2");
+        System.out.println(2);
+        System.out.println("new commit");
     }
 }
